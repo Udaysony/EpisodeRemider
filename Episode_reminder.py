2aca0683af6c572746ec0231d08f206bc44658fc
Udaysony <Soniuday111@gmail.com>
2018-12-23 18:46:31 -0800

    Update Episode_reminder.py

--- Episode_reminder.py
+++ Episode_reminder.py
@@ -1,9 +1,16 @@
+
+
+#  Script has to run manually for now. We can make a job_script to run it on specific time, everyday. 
+# Edit yyour faviourite episodes in list to get notifications.
+
 import urllib.request as ul
 import bs4 as bs
 import datetime
+import smtplib
 
 
-favourite_episodes = ["the big bang theory", "gotham","the flash"]  #Enter your favourite episode in list
+
+favourite_episodes = ["the big bang theory", "gotham"]  #Enter your favourite episode in list
 l = []
 
 tv_url = 'http://www.tvmaze.com'
@@ -15,8 +22,20 @@ soup = bs.BeautifulSoup(page,'lxml')
 for names in soup.find_all("span", {"class": "showname"}):
 	l.append(names.text)			#creates list of todays episodes
 
+sender = "abc@gmail.com"	
+password = "password"
+target = "xyz"
+
+server = smtplib.SMTP_SSL('smtp.gmail.com',465)
+server.ehlo()
+server.login(sender,password)
+
+
 for i in favourite_episodes:
 	if i.title() in l:
-		print("{} is going to air today".format(i)) # we can call a function which can send text reminder
+		msg = i + " is going to air today." 
+		server.sendmail(sender,target,msg)
+
 	
+server.quit()
 
